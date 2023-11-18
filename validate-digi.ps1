4bf403774a34126a9de0e828da819a640fef4e4d
Joey Collard <zetronboy@hotmail.com>
2023-11-18 11:45:41 -0800

    fix for user pass in ssh script ssh is untested until I get another digi onhand.

--- validate-digi.ps1
+++ validate-digi.ps1
@@ -1,24 +1,32 @@
 <#
-SUMMARY:
+## SUMMARY:
 Powershell script to validate the DIGI Connect EZ configuration against expected values.
 Does not program the DIGI, just checks your settings against validations.cfg
-Uses SSH to dump the config and itterates through each cfg key=value to replace jinja style {{variable}} it finds
-For validations it does a -ilike *validate* to to match within the config. 
+Will replace Jinja2 style {{variable}} it finds with key=value from ip.cfg
+Uses SSH to dump the config and itterates through each validation, testing against DIGI configuration.
+For validations it does a -ilike *<validation>* to to match within the config. 
 If a validation is prefixed with ! it will validate the string does not exist.
 
-AUTHOR: Joey Collard (NGA)
+## AUTHOR: Joey Collard
 
-SYNOPSIS:
+## SYNOPSIS:
 pass IP of configured DIGI as arg -ip or answer when prompted
+include -pass or be prompted, assume admin user if not passed as -user
 If no <IP>.cfg can be found, you will be prompted to enter parameters and a cfg file will be created. 
-After it exists, you would need to modify it directly or delete to change espected values for the DIGI.
-Reads from the DIGI, assumes login/pass admin/Pa55word!
+The default .cfg file creates key/value pairs are specific to my use and the validations.cfg.
+After <IP>.cfg exists, you would need to modify it directly or delete to change espected values for the DIGI.
+Reads from the DIGI, assumes login username admin if not supplied. You must supply the password for SSH.
 runs each of the validations.cfg lines after replacing {{key}} with values from <IP>.cfg, like a Jinja2 template.
+You can run the same validations.cfg against all your DIGI because they each have a <IP>.cfg with replacement values to expect.
 lines that dont match get a -FAIL. Edit the DIGI using web to correct and run again.
-Makes use of PLINK.EXE, part of a Putty install.
+Supports a -config arg that will use a local text file containing DIGI config rather than SSH. Used for development.
 
-EXAMPLE:
-powershell -f validate-digi.ps1 -ip 172.28.1.2
+## REQUIRES:
+PUTTY is required: Makes use of PLINK.EXE, part of a Putty install. Uses SSH to pull DIGI config. 
+You must enable the SSH Service on the DIGI (default setting) on port 22.
+
+## EXAMPLE:
+powershell -f validate-digi.ps1 -ip 172.28.1.2 -pass supersecretpass -user admin
 
 #>
 PARAM(
@@ -100,7 +108,7 @@ if ($config_path) #cmdline arg -config can override and read from disk instead o
 @"
 @echo off
 echo caching SSH cert before running script
-echo y | "$plink_path" $username@$ip "exit"
+echo y | "$plink_path" $user@$ip "exit"
 (
     timeout /t 2 > nul
     echo a
@@ -112,7 +120,7 @@ echo y | "$plink_path" $username@$ip "exit"
     echo exit
     timeout /t 1 > nul
     echo q
-) | "$plink_path" admin@$ip -pw "$password" -batch
+) | "$plink_path" $user@$ip -pw "$password" -batch
 "@ | out-file -FilePath .\ssh-command.bat -Encoding ascii
 
     #run the Batch command we created above to plink
